3425624a026775b593267297cabdc4a7f0c08138
Lionel <lionel@pop-os.localdomain>
2020-11-27 02:12:08 +0200

    creating custom Dynamic Input Component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from "../../../components/UI/Button/Button"
 import Spinner from "../../../components/UI/Spinner/Spinner"
 import classes from "./ContactData.module.css"
 import axios from "../../../axios-orders"
+import Input from "../../../components/UI/Input/Input"
 class ContactData extends Component {
   state = {
     name: "",
@@ -43,26 +44,26 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="name"
           placeholder="Your Name"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="email"
           name="email"
           placeholder="Your Mail"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="street"
           placeholder="Street"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="postalCode"
           placeholder="PostalCode"
